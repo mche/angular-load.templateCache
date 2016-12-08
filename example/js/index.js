762a57966c86ee0da53121ce55c4b2a053c4e28b
Mikhail Che <ernesto.chege@yandex.ru>
2016-12-08 16:59:17 +0500

    commit

--- example/js/index.js
+++ example/js/index.js
@@ -1,5 +1,5 @@
 angular.module('myApp', ['load.templateCache'])
 .controller('MyCtrl', function($scope, loadTemplateCache) {
-    loadTemplateCache.split(["https://raw.githubusercontent.com/mche/angular-load.templateCache/master/example/templates.html"]);
+    loadTemplateCache.split(["/templates.html"]);
 })
 ;
--- script.js
+++ script.js
@@ -18,7 +18,7 @@ angular.module('load.templateCache', [])
   };
   
   var re = {
-    mojo: /^@@@\s*([^\n]+)([^@]+)/gm
+    mojo: /^@@@\s*(.+)/gm //([^\n]+)
     
   };
   
@@ -26,15 +26,27 @@ angular.module('load.templateCache', [])
     done = done || 'complete';
     //~ return console.log(arr);
     arr.map(function(url, index) {
-      console.log(url, $http);
+      //~ console.log(url, $http);
       $http.get(url, {"cache": true}).then(function (resp) {
-        //~ $templateCache.put(key, resp.data);
-        while (result = re.mojo.exec(resp.data)) {
-          console.log(result);
+        var splt = resp.data.split(re.mojo);
+        console.log(splt);
+        //~ var result;
+        //~ while (result = re.mojo.exec(resp.data)) {
+          //~ console.log(result);
+        //~ }
+        while (splt.length > 0) {
+          var id = splt.shift();
+          if (id == "") continue;
+          var tpl = splt.shift();
+          console.log(id, "=>>>", tpl);
+          $templateCache.put(id, tpl);
         }
         var idx = arr.indexOf(url);
-        arr.slice(idx, 1);
-        if (arr.length == 0) arr[0] = done;
+        //~ arr.slice(idx, 1);
+        delete arr[idx];
+        
+        if (arr.join('') == "") arr[0] = done;
+        //~ console.log("Массив ", arr);
       });
     });
   };
