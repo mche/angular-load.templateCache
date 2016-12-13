3acf63ee94c59b1fabbf97a9fc02e8cd228b3c1e
Mikhail Che <ernesto.chege@yandex.ru>
2016-12-13 17:49:26 +0500

    commit

--- script.js
+++ script.js
@@ -35,15 +35,11 @@ angular.module('load.templateCache', [])
   };
   
   this.split = function(arr, flag_all) {// массив урлов (flag_all - returns $q.all(...)
-    var promise = arr.map(function(url, index) {
-      //~ console.log(url, $http);
-      return $http.get(url, {"cache": true}).then(function (resp) {
+    var promise = [];
+    
+    angular.forEach(arr, function(url, idx) {
+      var get = $http.get(url, {"cache": true}).then(function (resp) {
         var splt = resp.data.split(re.mojo);
-        //~ console.log(splt);
-        //~ var result;
-        //~ while (result = re.mojo.exec(resp.data)) {
-          //~ console.log(result);
-        //~ }
         while (splt.length > 0) {
           var id = splt.shift();
           if (id == "") continue;
@@ -51,13 +47,13 @@ angular.module('load.templateCache', [])
           //~ console.log("Template: ", id);
           $templateCache.put(id, tpl);
         }
-        var idx = arr.indexOf(url);
-        //~ arr.slice(idx, 1);
-        delete arr[idx];
-        
-        if (arr.join('') == "") arr.unshift(done);//arr[0] = done;
-        //~ console.log("Массив ", arr);
+        //~ var idx = arr.indexOf(url);
+        //~ delete arr[idx];
+        //~ if (arr.join('') == "") arr.unshift(done);//arr[0] = done;
       });
+      
+      promise.push(get);
+      
     });
     
     if (flag_all) return $q.all(promise);
